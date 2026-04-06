02c73bfc8e2b9ea231de86f4495c8e0b439f7ef0
kmkang <wowkmini15@gmail.com>
2026-04-06 17:02:32 +0900

    init(): 프로젝트 초기 설정

---
+++ src/main/java/net/coboogie/fillybackend/FillyBackendApplication.java
@@ -0,0 +1,13 @@
+package net.coboogie.fillybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FillyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FillyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/coboogie/fillybackend/FillyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.coboogie.fillybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FillyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
