b7524334db625aaa8369d37080160ff247d78cab
JAEMO-LIM <tjdwkddl@gmail.com>
2026-04-08 21:20:29 +0900

    refactor: BLIP 서비스 코드 수정 및 .gitignore 설정 추가

--- src/main/java/net/coboogie/fillybackend/vo/AiAnalysisResponse.java
+++ src/main/java/net/coboogie/fillybackend/vo/AiAnalysisResponse.java
@@ -1,4 +1,13 @@
 package net.coboogie.fillybackend.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class AiAnalysisResponse {
+    private String filename;
+    private String caption;
 }
