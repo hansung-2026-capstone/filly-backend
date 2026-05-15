fc78593eeb1d77df51b7c5f5a51969827da92204
JAEMO-LIM <tjdwkddl@gmail.com>
2026-05-15 15:10:06 +0900

    feat:일기 미디어 소유권 조회 추가

--- src/main/java/net/coboogie/diary/repository/DiaryMediaRepository.java
+++ src/main/java/net/coboogie/diary/repository/DiaryMediaRepository.java
@@ -4,6 +4,7 @@ import net.coboogie.vo.DiaryMediaVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * {@code diary_media} 테이블에 대한 JPA Repository.
@@ -15,4 +16,9 @@ public interface DiaryMediaRepository extends JpaRepository<DiaryMediaVO, Long> 
      * 특정 일기에 첨부된 미디어 목록을 조회한다.
      */
     List<DiaryMediaVO> findByDiary_Id(Long diaryId);
+
+    /**
+     * 특정 사용자의 특정 일기에 속한 미디어를 조회한다.
+     */
+    Optional<DiaryMediaVO> findByIdAndDiary_IdAndDiary_User_Id(Long id, Long diaryId, Long userId);
 }
