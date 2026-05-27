b5b5d51bce5014ad26c5089c5fe15ef9c7c1daf5
kmkang <wowkmini15@gmail.com>
2026-05-28 03:45:46 +0900

    test(DiaryServiceTest.java): 목록 조회 mock 메서드 갱신

--- src/test/java/net/coboogie/diary/service/DiaryServiceTest.java
+++ src/test/java/net/coboogie/diary/service/DiaryServiceTest.java
@@ -583,7 +583,7 @@ class DiaryServiceTest {
                 .id(2L).user(mockUser).rawContent("셋째 날").writtenAt(LocalDate.of(2026, 4, 3))
                 .createdAt(LocalDateTime.now()).build();
 
-        given(diaryEntryRepository.findByUser_IdAndWrittenAtBetweenOrderByWrittenAtAsc(
+        given(diaryEntryRepository.findWithMediaByUser_IdAndWrittenAtBetweenOrderByWrittenAtAsc(
                 userId, LocalDate.of(2026, 4, 1), LocalDate.of(2026, 4, 30)))
                 .willReturn(List.of(diary1, diary2));
 
@@ -602,7 +602,7 @@ class DiaryServiceTest {
         // given
         Long userId = 1L;
 
-        given(diaryEntryRepository.findByUser_IdAndWrittenAtBetweenOrderByWrittenAtAsc(
+        given(diaryEntryRepository.findWithMediaByUser_IdAndWrittenAtBetweenOrderByWrittenAtAsc(
                 userId, LocalDate.of(2026, 4, 1), LocalDate.of(2026, 4, 30)))
                 .willReturn(Collections.emptyList());
 
