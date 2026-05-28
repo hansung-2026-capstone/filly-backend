799b6527f29a4d8842037b913bb6252dd328509c
kmkang <wowkmini15@gmail.com>
2026-05-29 04:24:24 +0900

    feat(AvatarResponse.java): 아바타 공개 URL 응답 명시

--- src/main/java/net/coboogie/avatar/dto/AvatarResponse.java
+++ src/main/java/net/coboogie/avatar/dto/AvatarResponse.java
@@ -4,7 +4,7 @@ package net.coboogie.avatar.dto;
  * 아바타 생성 결과 응답 DTO.
  *
  * @param avatarHistoryId 생성된 아바타 이력 ID
- * @param avatarUrl       아바타 이미지 서명 URL (1시간 유효)
+ * @param avatarUrl       아바타 이미지 공개 URL
  */
 public record AvatarResponse(
         Long avatarHistoryId,
